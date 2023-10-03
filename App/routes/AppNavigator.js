b69a3bd9d27fabe5d119602c17d3bf0041aac680
patemit <patemit@oregonstate.edu>
2023-10-03 15:11:28 -0400

    Updated Color and Icons of Bottom Navigation Items

--- App/routes/AppNavigator.js
+++ App/routes/AppNavigator.js
@@ -4,16 +4,62 @@ import Home from "../screens/Home";
 import Chat from "../screens/Chat";
 import Applications from "../screens/Applications";
 import Settings from "../screens/Settings";
+import Feathericons from "@expo/vector-icons/Feather";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Chat" component={Chat} />
-      <Tab.Screen name="Application" component={Applications} />
-      <Tab.Screen name="Settings" component={Settings} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName, IconComponent;
+
+          // Select an icon name based on the route name
+          if (route.name === "Home") {
+            iconName = focused ? "compass" : "compass";
+            IconComponent = Feathericons;
+          } else if (route.name === "Applications") {
+            iconName = focused ? "plus-circle" : "plus-circle";
+            IconComponent = Feathericons;
+          } else if (route.name === "Chat") {
+            iconName = focused
+              ? "person-circle-outline"
+              : "person-circle-outline";
+            IconComponent = Ionicons;
+          } else if (route.name === "Settings") {
+            iconName = focused ? "settings" : "settings-outline";
+            IconComponent = Ionicons;
+          }
+
+          if (!IconComponent || !iconName) return null;
+
+          // Return an Icon component with the selected iconName
+          return <IconComponent name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#ebae52", // Set active tab color
+        tabBarInactiveTintColor: "gray", // Set inactive tab color
+        tabBarStyle: [
+          {
+            display: "flex",
+          },
+          null,
+        ],
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} options={{ tabBarLabel: "" }} />
+      <Tab.Screen
+        name="Applications"
+        component={Applications}
+        options={{ tabBarLabel: "" }}
+      />
+      <Tab.Screen name="Chat" component={Chat} options={{ tabBarLabel: "" }} />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{ tabBarLabel: "" }}
+      />
     </Tab.Navigator>
   );
 };

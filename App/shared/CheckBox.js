8dcc7c5ddd8828546cff84d72e6b594e8bf31b30
patemit <patelmit8292@gmail.com>
2023-11-10 13:23:00 -0500

    Updated Styling for CheckListItems

--- App/shared/CheckBox.js
+++ App/shared/CheckBox.js
@@ -3,7 +3,7 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const CustomCheckbox = ({ isChecked, onToggle }) => {
   return (
-    <TouchableOpacity style={styles.checkbox} onPress={onToggle}>
+    <TouchableOpacity style={styles.checkbox} onPress={onToggle} activeOpacity={1}>
       {isChecked && <View style={styles.checked} />}
     </TouchableOpacity>
   );
@@ -27,27 +27,31 @@ const styles = StyleSheet.create({
   item: {
     flexDirection: "row",
     alignItems: "center",
+    marginVertical: 5,
     // additional styles
   },
   checkbox: {
-    width: 10,
-    height: 10,
+    width: 20,
+    height: 20,
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: "black",
+    borderColor: "#10798B",
     justifyContent: "center",
     alignItems: "center",
     marginRight: 10,
   },
   checked: {
-    width: 12,
-    height: 12,
-    borderRadius: 6,
-    backgroundColor: "black",
+    width: 15,
+    height: 15,
+    borderRadius: 10,
+    backgroundColor: "#10798B",
   },
 
   text: {
-    color: "red",
+    fontFamily: "karla-regular",
+    color: "#171B1C",
+    fontSize: 16,
+    letterSpacing: -0.16,
   },
 });
 

052ac07bc8a8998fea7f16b0e6ec95af2aa2df36
patemit <patelmit8292@gmail.com>
2023-11-27 15:43:51 -0500

    Created Dropdown in addServicesPage

--- App/screens/addServicesPage.js
+++ App/screens/addServicesPage.js
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import globalstyles from "../shared/globalStyles";
 
 const ServicesPage = () => {
+  const [dropdownsOpen, setDropdownsOpen] = useState({
+    firstDropdown: false,
+    secondDropdown: false,
+    // Add more dropdown states as needed
+  });
+
+  // Function to toggle a specific dropdown
+  const toggleDropdown = (dropdownKey) => {
+    setDropdownsOpen((prevState) => ({
+      ...prevState,
+      [dropdownKey]: !prevState[dropdownKey],
+    }));
+  };
   return (
     <SafeAreaView style={globalstyles.container}>
       <View style={{ margin: 40 }}></View>
@@ -12,6 +25,30 @@ const ServicesPage = () => {
       </View>
       <Text style={globalstyles.subHeader}>What services do you provide?</Text>
       <View>
+        <TouchableOpacity onPress={() => toggleDropdown('firstDropdown')}>
+          <Text>Show Items</Text>
+        </TouchableOpacity>
+        {dropdownsOpen.firstDropdown && (
+          <View>
+            {/* List your items here */}
+            <Text>Item 1</Text>
+            <Text>Item 2</Text>
+            <Text>Item 3</Text>
+            {/* Add more items as needed */}
+          </View>
+        )}
+        <TouchableOpacity onPress={() => toggleDropdown('secondDropdown')}>
+          <Text>Show Items</Text>
+        </TouchableOpacity>
+        {dropdownsOpen.secondDropdown && (
+          <View>
+            {/* List your items here */}
+            <Text>Item 1</Text>
+            <Text>Item 2</Text>
+            <Text>Item 3</Text>
+            {/* Add more items as needed */}
+          </View>
+        )}
       </View>
     </SafeAreaView>
   );

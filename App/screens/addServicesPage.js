856c025d01a42db2f2e6add7559007b4cd3a7ef5
patemit <patelmit8292@gmail.com>
2023-11-27 15:20:33 -0500

    Updated addServicesPage

--- App/screens/addServicesPage.js
+++ App/screens/addServicesPage.js
@@ -1,15 +1,22 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import globalstyles from "../shared/globalStyles";
 
 const ServicesPage = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={globalstyles.container}>
+      <View style={{ margin: 40 }}></View>
+      <View style={globalstyles.headerContainer}>
+        <Text style={globalstyles.header}>Add your services</Text>
+      </View>
+      <Text style={globalstyles.subHeader}>What services do you provide?</Text>
       <View>
-        <Text>add a service</Text>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default ServicesPage;

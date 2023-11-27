2e4bb1815c2bf5650fe94215d449abd03d4703e8
patemit <patelmit8292@gmail.com>
2023-11-27 17:07:49 -0500

    Created Dropdown options within the Services Page

--- App/screens/addServicesPage.js
+++ App/screens/addServicesPage.js
@@ -2,19 +2,25 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import globalstyles from "../shared/globalStyles";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 const ServicesPage = () => {
-  const [dropdownsOpen, setDropdownsOpen] = useState({
-    firstDropdown: false,
-    secondDropdown: false,
-    // Add more dropdown states as needed
+  const [dropdowns, setDropdowns] = useState({
+    dropdown1: false,
+    dropdown2: false,
+    dropdown3: false,
+    dropdown4: false,
+    dropdown5: false,
+    dropdown6: false,
+    dropdown7: false,
+    dropdown8: false,
+    // Add more dropdowns as needed
   });
 
-  // Function to toggle a specific dropdown
-  const toggleDropdown = (dropdownKey) => {
-    setDropdownsOpen((prevState) => ({
+  const toggleDropdown = (key) => {
+    setDropdowns((prevState) => ({
       ...prevState,
-      [dropdownKey]: !prevState[dropdownKey],
+      [key]: !prevState[key],
     }));
   };
   return (
@@ -24,29 +30,165 @@ const ServicesPage = () => {
         <Text style={globalstyles.header}>Add your services</Text>
       </View>
       <Text style={globalstyles.subHeader}>What services do you provide?</Text>
-      <View>
-        <TouchableOpacity onPress={() => toggleDropdown('firstDropdown')}>
-          <Text>Show Items</Text>
+      <View style={{ marginTop: 20 }}>
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={() => toggleDropdown("dropdown1")}
+          style={styles.dropdownButton}
+        >
+          <Text style={styles.text}>Community</Text>
+          <Icon
+            name={
+              dropdowns.dropdown1 ? "horizontal-rule" : "keyboard-arrow-right"
+            }
+            size={28}
+            style={styles.icon}
+          />
         </TouchableOpacity>
-        {dropdownsOpen.firstDropdown && (
+        {dropdowns.dropdown1 && (
           <View>
-            {/* List your items here */}
             <Text>Item 1</Text>
             <Text>Item 2</Text>
-            <Text>Item 3</Text>
-            {/* Add more items as needed */}
           </View>
         )}
-        <TouchableOpacity onPress={() => toggleDropdown('secondDropdown')}>
-          <Text>Show Items</Text>
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={() => toggleDropdown("dropdown2")}
+          style={styles.dropdownButton}
+        >
+          <Text style={styles.text}>Finance</Text>
+          <Icon
+            name={
+              dropdowns.dropdown2 ? "horizontal-rule" : "keyboard-arrow-right"
+            }
+            size={28}
+            style={styles.icon}
+          />
         </TouchableOpacity>
-        {dropdownsOpen.secondDropdown && (
+        {dropdowns.dropdown2 && (
           <View>
-            {/* List your items here */}
             <Text>Item 1</Text>
             <Text>Item 2</Text>
-            <Text>Item 3</Text>
-            {/* Add more items as needed */}
+          </View>
+        )}
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={() => toggleDropdown("dropdown3")}
+          style={styles.dropdownButton}
+        >
+          <Text style={styles.text}>Food</Text>
+          <Icon
+            name={
+              dropdowns.dropdown3 ? "horizontal-rule" : "keyboard-arrow-right"
+            }
+            size={28}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        {dropdowns.dropdown3 && (
+          <View>
+            <Text>Item 1</Text>
+            <Text>Item 2</Text>
+          </View>
+        )}
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={() => toggleDropdown("dropdown4")}
+          style={styles.dropdownButton}
+        >
+          <Text style={styles.text}>Health</Text>
+          <Icon
+            name={
+              dropdowns.dropdown4 ? "horizontal-rule" : "keyboard-arrow-right"
+            }
+            size={28}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        {dropdowns.dropdown4 && (
+          <View>
+            <Text>Item 1</Text>
+            <Text>Item 2</Text>
+          </View>
+        )}
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={() => toggleDropdown("dropdown5")}
+          style={styles.dropdownButton}
+        >
+          <Text style={styles.text}>Hygiene</Text>
+          <Icon
+            name={
+              dropdowns.dropdown5 ? "horizontal-rule" : "keyboard-arrow-right"
+            }
+            size={28}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        {dropdowns.dropdown5 && (
+          <View>
+            <Text>Item 1</Text>
+            <Text>Item 2</Text>
+          </View>
+        )}
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={() => toggleDropdown("dropdown6")}
+          style={styles.dropdownButton}
+        >
+          <Text style={styles.text}>Shelter</Text>
+          <Icon
+            name={
+              dropdowns.dropdown6 ? "horizontal-rule" : "keyboard-arrow-right"
+            }
+            size={28}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        {dropdowns.dropdown6 && (
+          <View>
+            <Text>Item 1</Text>
+            <Text>Item 2</Text>
+          </View>
+        )}
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={() => toggleDropdown("dropdown7")}
+          style={styles.dropdownButton}
+        >
+          <Text style={styles.text}>Work & Learn</Text>
+          <Icon
+            name={
+              dropdowns.dropdown7 ? "horizontal-rule" : "keyboard-arrow-right"
+            }
+            size={28}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        {dropdowns.dropdown7 && (
+          <View>
+            <Text>Item 1</Text>
+            <Text>Item 2</Text>
+          </View>
+        )}
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={() => toggleDropdown("dropdown8")}
+          style={styles.dropdownButton}
+        >
+          <Text style={styles.text}>Other</Text>
+          <Icon
+            name={
+              dropdowns.dropdown8 ? "horizontal-rule" : "keyboard-arrow-right"
+            }
+            size={28}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        {dropdowns.dropdown8 && (
+          <View>
+            <Text>Item 1</Text>
+            <Text>Item 2</Text>
           </View>
         )}
       </View>
@@ -54,6 +196,23 @@ const ServicesPage = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 24,
+    fontFamily: "gabarito-semibold",
+    color: "#1A4674",
+    marginVertical: 10,
+  },
+  icon: {
+    color: "#1A4674",
+    marginRight: 10,
+  },
+  dropdownButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+  },
+});
 
 export default ServicesPage;

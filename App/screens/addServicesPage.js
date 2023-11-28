f95af53ac9a2a48e6be6c328c1450c4d273dc6da
patemit <patelmit8292@gmail.com>
2023-11-28 14:36:48 -0500

    Chage style of OptionText when selected

--- App/screens/addServicesPage.js
+++ App/screens/addServicesPage.js
@@ -97,11 +97,20 @@ const ServicesPage = () => {
             <View style={{ paddingHorizontal: 40, marginVertical: 5 }}>
               {communityOptions.map((option, index) => (
                 <TouchableOpacity
+                  activeOpacity={1}
                   key={index}
                   onPress={() => handleSelect(option, "Community")}
                   style={styles.optionsContainer}
                 >
-                  <Text style={styles.optionsText}>{option}</Text>
+                  <Text
+                    style={[
+                      styles.optionText,
+                      selectedOptions.Community.includes(option) &&
+                        styles.selectedOptionText,
+                    ]}
+                  >
+                    {option}
+                  </Text>
                   {selectedOptions.Community.includes(option) && (
                     <Icon name="check" size={20} style={styles.checkIcon} />
                   )}
@@ -127,11 +136,20 @@ const ServicesPage = () => {
             <View style={{ paddingHorizontal: 40, marginVertical: 5 }}>
               {financeOptions.map((option, index) => (
                 <TouchableOpacity
+                  activeOpacity={1}
                   key={index}
                   onPress={() => handleSelect(option, "Finance")}
                   style={styles.optionsContainer}
                 >
-                  <Text style={styles.optionsText}>{option}</Text>
+                  <Text
+                    style={[
+                      styles.optionText,
+                      selectedOptions.Finance.includes(option) &&
+                        styles.selectedOptionText,
+                    ]}
+                  >
+                    {option}
+                  </Text>
                   {selectedOptions.Finance.includes(option) && (
                     <Icon name="check" size={20} style={styles.checkIcon} />
                   )}
@@ -155,11 +173,20 @@ const ServicesPage = () => {
             <View style={{ paddingHorizontal: 40, marginVertical: 5 }}>
               {foodOptions.map((option, index) => (
                 <TouchableOpacity
+                  activeOpacity={1}
                   key={index}
                   onPress={() => handleSelect(option, "Food")}
                   style={styles.optionsContainer}
                 >
-                  <Text style={styles.optionsText}>{option}</Text>
+                  <Text
+                    style={[
+                      styles.optionText,
+                      selectedOptions.Food.includes(option) &&
+                        styles.selectedOptionText,
+                    ]}
+                  >
+                    {option}
+                  </Text>
                   {selectedOptions.Food.includes(option) && (
                     <Icon name="check" size={20} style={styles.checkIcon} />
                   )}
@@ -185,11 +212,20 @@ const ServicesPage = () => {
             <View style={{ paddingHorizontal: 40, marginVertical: 5 }}>
               {healthOptions.map((option, index) => (
                 <TouchableOpacity
+                  activeOpacity={1}
                   key={index}
                   onPress={() => handleSelect(option, "Health")}
                   style={styles.optionsContainer}
                 >
-                  <Text style={styles.optionsText}>{option}</Text>
+                  <Text
+                    style={[
+                      styles.optionText,
+                      selectedOptions.Health.includes(option) &&
+                        styles.selectedOptionText,
+                    ]}
+                  >
+                    {option}
+                  </Text>
                   {selectedOptions.Health.includes(option) && (
                     <Icon name="check" size={20} style={styles.checkIcon} />
                   )}
@@ -215,11 +251,20 @@ const ServicesPage = () => {
             <View style={{ paddingHorizontal: 40, marginVertical: 5 }}>
               {hygieneOptions.map((option, index) => (
                 <TouchableOpacity
+                  activeOpacity={1}
                   key={index}
                   onPress={() => handleSelect(option, "Hygiene")}
                   style={styles.optionsContainer}
                 >
-                  <Text style={styles.optionsText}>{option}</Text>
+                  <Text
+                    style={[
+                      styles.optionText,
+                      selectedOptions.Hygiene.includes(option) &&
+                        styles.selectedOptionText,
+                    ]}
+                  >
+                    {option}
+                  </Text>
                   {selectedOptions.Hygiene.includes(option) && (
                     <Icon name="check" size={20} style={styles.checkIcon} />
                   )}
@@ -245,11 +290,20 @@ const ServicesPage = () => {
             <View style={{ paddingHorizontal: 40, marginVertical: 5 }}>
               {shelterOptions.map((option, index) => (
                 <TouchableOpacity
+                  activeOpacity={1}
                   key={index}
                   onPress={() => handleSelect(option, "Shelter")}
                   style={styles.optionsContainer}
                 >
-                  <Text style={styles.optionsText}>{option}</Text>
+                  <Text
+                    style={[
+                      styles.optionText,
+                      selectedOptions.Shelter.includes(option) &&
+                        styles.selectedOptionText,
+                    ]}
+                  >
+                    {option}
+                  </Text>
                   {selectedOptions.Shelter.includes(option) && (
                     <Icon name="check" size={20} style={styles.checkIcon} />
                   )}
@@ -277,11 +331,20 @@ const ServicesPage = () => {
             <View style={{ paddingHorizontal: 40, marginVertical: 5 }}>
               {work_learnOptions.map((option, index) => (
                 <TouchableOpacity
+                  activeOpacity={1}
                   key={index}
                   onPress={() => handleSelect(option, "Work_Learn")}
                   style={styles.optionsContainer}
                 >
-                  <Text style={styles.optionsText}>{option}</Text>
+                  <Text
+                    style={[
+                      styles.optionText,
+                      selectedOptions.Work_Learn.includes(option) &&
+                        styles.selectedOptionText,
+                    ]}
+                  >
+                    {option}
+                  </Text>
                   {selectedOptions.Work_Learn.includes(option) && (
                     <Icon name="check" size={20} style={styles.checkIcon} />
                   )}
@@ -307,13 +370,22 @@ const ServicesPage = () => {
             <View style={{ paddingHorizontal: 40, marginVertical: 5 }}>
               {otherOptions.map((option, index) => (
                 <TouchableOpacity
+                  activeOpacity={1}
                   key={index}
                   onPress={() => handleSelect(option, "Other")}
                   style={styles.optionsContainer}
                 >
-                  <Text style={styles.optionsText}>{option}</Text>
+                  <Text
+                    style={[
+                      styles.optionText,
+                      selectedOptions.Other.includes(option) &&
+                        styles.selectedOptionText,
+                    ]}
+                  >
+                    {option}
+                  </Text>
                   {selectedOptions.Other.includes(option) && (
-                    <Icon name="check" size={20} style={styles.checkIcon} />
+                    <Icon name="check" size={18} style={styles.checkIcon} />
                   )}
                 </TouchableOpacity>
               ))}
@@ -358,16 +430,23 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    borderWidth: 1,
   },
 
-  optionsText: {
+  optionText: {
     paddingHorizontal: 5,
     paddingVertical: 10,
     color: "#202423",
     fontSize: 16,
     fontFamily: "karla-regular",
     letterSpacing: -0.16,
+  },
+
+  selectedOptionText: {
+    fontFamily: "karla-bold",
+  },
+
+  checkIcon: {
+    color: "#202423",
   },
 });
 

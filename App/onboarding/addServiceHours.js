9b4d43189ee70224417cba9be702d102627b3b47
patemit <patelmit8292@gmail.com>
2023-11-29 15:18:09 -0500

    Added save and i'll do later buttons

--- App/onboarding/addServiceHours.js
+++ App/onboarding/addServiceHours.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { Text, View, ScrollView, StyleSheet, TextInput } from "react-native";
+import {
+  Text,
+  View,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import globalstyles from "../shared/globalStyles";
 
@@ -12,13 +19,13 @@ const ServiceHours = ({ route }) => {
   const [serviceDescription, setServiceDescription] = useState("");
 
   return (
-    <SafeAreaView style={[globalstyles.container, {marginHorizontal: 5}]}>
+    <SafeAreaView style={[globalstyles.container, { marginHorizontal: 5 }]}>
       <ScrollView>
         <View style={{ margin: 40 }} />
         <View style={globalstyles.headerContainer}>
           <Text style={globalstyles.header}>Add service hours</Text>
         </View>
-        <View style={{ marginBottom: 10 }}>
+        <View style={{ marginBottom: 5 }}>
           <Text style={globalstyles.subHeader}>Add your service hours</Text>
         </View>
         {/* Display the selected options here */}
@@ -34,35 +41,47 @@ const ServiceHours = ({ route }) => {
                   style={styles.servicesText}
                 >{`${category} > ${option}`}</Text>
                 <TextInput
-                  style={[globalstyles.textInput, {height: 45, paddingVertical: 0}]}
+                  style={[
+                    globalstyles.textInput,
+                    { height: 45, paddingVertical: 0 },
+                  ]}
                   placeholder={"Service name (optional)"}
                   value={serviceName}
                   onChange={setServiceName}
                   // You can add more props to TextInput as needed
                 />
                 <TextInput
-                  style={[globalstyles.textInput, {height: 45}]}
+                  style={[globalstyles.textInput, { height: 45 }]}
                   placeholder={"Location"}
                   value={serviceLocation}
                   onChange={setServiceLocation}
                   // You can add more props to TextInput as needed
                 />
                 <TextInput
-                  style={[globalstyles.textInput, {height: 45, paddingVertical: 0}]}
+                  style={[
+                    globalstyles.textInput,
+                    { height: 45, paddingVertical: 0 },
+                  ]}
                   placeholder={"Opening Hours"}
                   value={openingHours}
                   onChange={setOpeningHours}
                   // You can add more props to TextInput as needed
                 />
                 <TextInput
-                  style={[globalstyles.textInput, {height: 45, paddingVertical: 0}]}
+                  style={[
+                    globalstyles.textInput,
+                    { height: 45, paddingVertical: 0 },
+                  ]}
                   placeholder={"Closing Hours"}
                   value={closingHours}
                   onChange={setClosingHours}
                   // You can add more props to TextInput as needed
                 />
                 <TextInput
-                  style={[globalstyles.textInput, {height: 45, paddingVertical: 0}]}
+                  style={[
+                    globalstyles.textInput,
+                    { height: 45, paddingVertical: 0 },
+                  ]}
                   placeholder={"Service description (optional)"}
                   value={serviceDescription}
                   onChange={setServiceDescription}
@@ -71,6 +90,18 @@ const ServiceHours = ({ route }) => {
               </View>
             ))
           )}
+        <TouchableOpacity
+          // disabled={!serviceLocation || !openingHours || !closingHours}
+          style={[
+            globalstyles.buttonContainer,
+            { backgroundColor: "#10798B", marginTop: 20, marginBottom: 10 },
+          ]}
+        >
+          <Text style={[globalstyles.buttonText, { color: "#fff" }]}>Save</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={globalstyles.buttonContainer}>
+          <Text style={globalstyles.buttonText}>I'll do this later</Text>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   );
@@ -78,16 +109,16 @@ const ServiceHours = ({ route }) => {
 
 const styles = StyleSheet.create({
   servicesContainer: {
-    borderWidth: 1,
     justifyContent: "center",
+    marginVertical: 10,
   },
 
   servicesText: {
     color: "#030E07",
     fontSize: 24,
     fontFamily: "gabarito-medium",
+    marginBottom: 5,
   },
-
 });
 
 export default ServiceHours;

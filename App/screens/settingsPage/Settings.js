f49f5d9eb1222c023b7d8b4d107603638615a7e9
patemit <patelmit8292@gmail.com>
2023-12-11 18:33:22 -0500

    Added options  to Settings Page

--- App/screens/settingsPage/Settings.js
+++ App/screens/settingsPage/Settings.js
@@ -1,15 +1,30 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import globalstyles from "../../shared/globalStyles";
 import SearchComponent from "../../shared/SearchHeader";
 
 export default function Settings() {
+  const pages = [
+    { name: "Org Profile", label: "Organization Profile" },
+    { name: "Account", label: "My Account" },
+    { name: "Notifications", label: "Notifications" },
+    { name: "Help", label: "Help" },
+    { name: "Log Out", label: "Log Out" },
+  ];
+
   return (
     <SafeAreaView style={globalstyles.container}>
       <SearchComponent />
-      <View style={{marginVertical: 5}}>
+      <View style={{ marginVertical: 5 }}>
         <Text style={globalstyles.title}>Settings</Text>
+      </View>
+      <View>
+        {pages.map((page, index) => (
+          <TouchableOpacity key={index} style={styles.container}>
+            <Text>{page.label}</Text>
+          </TouchableOpacity>
+        ))}
       </View>
     </SafeAreaView>
   );
@@ -17,9 +32,9 @@ export default function Settings() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    borderWidth: 1,
+    marginVertical:5,
+    padding: 20,
+    borderRadius: 15,
   },
 });

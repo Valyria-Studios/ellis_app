2986db5ec31b03d6b5c9dc0164431e1c3b453392
patemit <patelmit8292@gmail.com>
2023-12-13 02:18:03 -0500

    Icons added to options in Request Services Page

--- App/screens/RequestServicePage.js
+++ App/screens/RequestServicePage.js
@@ -8,39 +8,107 @@ import {
   TouchableOpacity,
 } from "react-native";
 import globalstyles from "../shared/globalStyles";
-import Icon from "@expo/vector-icons/Ionicons";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 const RequestService = () => {
   const serviceCategories = [
-    { name: "Food", options: ["Emergency Food", "Groceries (Food Pantry)"] },
+    {
+      name: "Food",
+      options: [
+        { name: "Emergency Food", icon: "bento", library: "MaterialIcons" },
+        {
+          name: "Groceries (Food Pantry)",
+          icon: "local-grocery-store",
+          library: "MaterialIcons",
+        },
+      ],
+    },
     {
       name: "Health",
       options: [
-        "Urgent Care",
-        "Mental Health Urgent Care",
-        "Mental Health Services",
-        "Women's Health",
+        { name: "Urgent Care", icon: "star-of-life", library: "FontAwesome5" },
+        {
+          name: "Mental Health Urgent Care",
+          icon: "clinic-medical",
+          library: "FontAwesome5",
+        },
+        {
+          name: "Mental Health Services",
+          icon: "brain",
+          library: "FontAwesome5",
+        },
+        { name: "Women's Health", icon: "md-female", library: "Ionicons" },
       ],
     },
     {
       name: "Housing",
-      options: ["Temporary Housing", "Permanent Supportive Housing"],
+      options: [
+        {
+          name: "Temporary Housing",
+          icon: "home-search-outline",
+          library: "MaterialCommunityIcons",
+        },
+        {
+          name: "Permanent Supportive Housing",
+          icon: "home",
+          library: "FontAwesome5",
+        },
+      ],
     },
     {
       name: "Legal",
-      options: ["Civil Defense", "Criminal Defense", "Evicition Prevention"],
+      options: [
+        { name: "Civil Litigation", icon: "groups", library: "MaterialIcons" },
+        {
+          name: "Criminal Defense",
+          icon: "briefcase-outline",
+          library: "MaterialCommunityIcons",
+        },
+        {
+          name: "Evicition Prevention",
+          icon: "bed-king-outline",
+          library: "MaterialCommunityIcons",
+        },
+      ],
     },
   ];
 
+  const renderIcon = (iconName, library) => {
+    switch (library) {
+      case "Ionicons":
+        return <Ionicons name={iconName} size={20} style={styles.icon} />;
+      case "MaterialIcons":
+        return <MaterialIcons name={iconName} size={20} style={styles.icon} />;
+      case "FontAwesome5":
+        return <FontAwesome5 name={iconName} size={20} style={styles.icon} />;
+      case "MaterialCommunityIcons":
+        return (
+          <MaterialCommunityIcons
+            name={iconName}
+            size={20}
+            style={styles.icon}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
-    <ScrollView style={globalstyles.container}>
+    <ScrollView
+      style={globalstyles.container}
+      showsVerticalScrollIndicator={false}
+    >
       <View>
         <Text style={[globalstyles.title, { fontSize: 36 }]}>
           Service Requests
         </Text>
         <View style={[globalstyles.searchSection, { marginVertical: 15 }]}>
           <View style={globalstyles.searchContainer}>
-            <Icon
+            <Ionicons
               name="search-outline"
               size={20}
               color="#616a6c"
@@ -63,11 +131,19 @@ const RequestService = () => {
           <View key={index}>
             <Text style={styles.subHeader}>{category.name}</Text>
             {category.options.map((options, idx) => (
-              <TouchableOpacity key={idx} style={styles.container}>
-                <View style={[globalstyles.optionsContainer, {padding: 20}]}>
-                  <Text style={globalstyles.optionsText}>{options}</Text>
-                </View>
-              </TouchableOpacity>
+              <View style={{ marginHorizontal: 10 }}>
+                <TouchableOpacity key={idx} style={styles.container}>
+                  <View
+                    style={[
+                      globalstyles.optionsContainer,
+                      { paddingVertical: 16, justifyContent: "flex-start" },
+                    ]}
+                  >
+                    {renderIcon(options.icon, options.library)}
+                    <Text style={globalstyles.optionsText}>{options.name}</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
             ))}
           </View>
         ))}
@@ -89,6 +165,10 @@ const styles = StyleSheet.create({
     marginVertical: 5,
     borderRadius: 15,
     backgroundColor: "#fff",
+  },
+
+  icon: {
+    color: "#094852",
   },
 });
 

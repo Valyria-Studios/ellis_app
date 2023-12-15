5dfcb65bab7ec013b3fce9f57c815fa6a01cc1b8
patemit <patelmit8292@gmail.com>
2023-12-15 12:21:04 -0700

    Created Template for Organization Page

--- App/screens/settingsPage/OrganizationProfile.js
+++ App/screens/settingsPage/OrganizationProfile.js
@@ -1,11 +1,30 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
+import globalstyles from "../../shared/globalStyles";
 
 const OrgProfile = () => {
   return (
-    <View>
-      <Text>hello</Text>
-    </View>
+    <ScrollView style={globalstyles.container}>
+      <View>
+        <Text style={[globalstyles.title, { fontSize: 30 }]}>hello</Text>
+      </View>
+      <View style={{ marginVertical: 10 }}>
+        <Text>Some Image</Text>
+      </View>
+      <View style={{ marginVertical: 10 }}>
+        <Text>Address </Text>
+        <Text>Phone Number</Text>
+        <Text>Website</Text>
+        <Text>Social Media</Text>
+        <Text>Tags</Text>
+      </View>
+      <View>
+        <Text>Services</Text>
+        <View>
+          <Text>Cards for each Service</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 

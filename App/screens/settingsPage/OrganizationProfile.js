429627164a4114d94db23f1694c3593c22347b3d
patemit <patelmit8292@gmail.com>
2023-12-20 14:46:48 -0500

    Added edit button to the header

--- App/screens/settingsPage/OrganizationProfile.js
+++ App/screens/settingsPage/OrganizationProfile.js
@@ -9,7 +9,7 @@ import {
 } from "react-native";
 import globalstyles from "../../shared/globalStyles";
 
-const OrgProfile = () => {
+const OrgProfile = ({ navigation }) => {
   const [account, setAccount] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
   const [editableAddress, setEditableAddress] = useState("");
@@ -32,6 +32,18 @@ const OrgProfile = () => {
         console.error(error);
       });
   }, []);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={handleEditSave} style={styles.headerButton}>
+          <Text style={styles.headerButtonText}>
+            {isEditing ? "Save" : "Edit"}
+          </Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation, isEditing]);
 
   const updateAccountDetails = async () => {
     try {
@@ -82,12 +94,6 @@ const OrgProfile = () => {
 
   return (
     <ScrollView style={globalstyles.container}>
-      <TouchableOpacity onPress={handleEditSave} style={styles.editButton}>
-        <Text>{isEditing ? "Save" : "Edit"}</Text>
-      </TouchableOpacity>
-
-      {/* ... other views ... */}
-
       <View style={{ marginVertical: 10 }}>
         <Text>Address</Text>
         <TextInput
@@ -118,47 +124,35 @@ const OrgProfile = () => {
 };
 
 const styles = StyleSheet.create({
-  editButton: { alignItems: "flex-end", padding: 10 },
-  editable: { borderWidth: 1, borderColor: "gray", padding: 5, margin: 5 },
-  nonEditable: { padding: 5, margin: 5 },
+  editButton: {
+    alignItems: "flex-end",
+    padding: 10,
+  },
+
+  editable: {
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 5,
+    margin: 5,
+  },
+
+  nonEditable: {
+    padding: 5,
+    margin: 5,
+  },
+
+  headerButton: {
+    marginRight: 15,
+    padding: 10,
+    borderRadius: 25,
+    backgroundColor: "#10798B",
+  },
+
+  headerButtonText: {
+    color: "#fff",
+    fontFamily: "gabarito-regular",
+    fontSize: 16,
+  },
 });
 
 export default OrgProfile;
-
-// import React, { useState, useEffect } from "react";
-// import { Text, View, ScrollView } from "react-native";
-// import globalstyles from "../../shared/globalStyles";
-
-// const OrgProfile = () => {
-//   const [account, setAccount] = useState(null);
-
-//   useEffect(() => {
-//     fetch("http://localhost:3000/Accounts")
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setAccount(data);
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//       });
-//   }, []);
-
-//   return (
-//     <ScrollView style={globalstyles.container}>
-//       <View>
-//         <Text style={[globalstyles.title, { fontSize: 30 }]}>hello</Text>
-//       </View>
-//       <View style={{ marginVertical: 10 }}>
-//         <Text>Some Image</Text>
-//       </View>
-//       <View style={{ marginVertical: 10 }}>
-//         <Text>Address</Text>
-//         <Text>Phone Number</Text>
-
-//       </View>
-
-//     </ScrollView>
-//   );
-// };
-
-// export default OrgProfile;

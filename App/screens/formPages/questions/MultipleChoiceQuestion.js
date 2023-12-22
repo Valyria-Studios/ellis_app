2f1d8bb42c57219b784e66deadc9923bf93490dc
patemit <patelmit8292@gmail.com>
2023-12-22 13:30:42 -0500

    Updated look of Options for Multiple Choice Question

--- App/screens/formPages/questions/MultipleChoiceQuestion.js
+++ App/screens/formPages/questions/MultipleChoiceQuestion.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
+import Icon from "@expo/vector-icons/MaterialIcons";
 
 const MultipleChoiceQuestion = ({ question, options }) => {
   const [selectedOption, setSelectedOption] = useState("");
@@ -12,15 +13,63 @@ const MultipleChoiceQuestion = ({ question, options }) => {
   }));
 
   return (
-    <View>
-      <Text>{question}</Text>
-      <RNPickerSelect
-        onValueChange={(value) => setSelectedOption(value)}
-        items={pickerItems}
-        placeholder={{ label: "Select an option...", value: null }}
-      />
+    <View style={styles.container}>
+      <Text style={styles.question}>{question}</Text>
+      <View style={styles.optionContainer}>
+        <RNPickerSelect
+          onValueChange={(value) => setSelectedOption(value)}
+          items={pickerItems}
+          placeholder={{ label: "Select an option", value: null }}
+          style={{
+            input: styles.input, // Common style for both iOS and Android
+            placeholder: styles.placeholder,
+            icon: styles.icon
+          }}
+          useNativeAndroidPickerStyle={false}
+          Icon={() => (
+            <Icon name="keyboard-arrow-right" size={24} color="#094852" />
+          )} //Icon not lined up in center //
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    // Add more styles for the container
+  },
+  question: {
+    fontSize: 16,
+    marginBottom: 5,
+    // Add more styles for the question
+  },
+
+  optionContainer: {
+    backgroundColor: "#fff",
+    padding: 15,
+    borderRadius: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  icon: {
+    alignItems: 'center'
+  },
+
+  input: {
+    fontSize: 16,
+    fontFamily: "karla-regular",
+    color: "#051A2E",
+    // Common styles for picker input
+  },
+  placeholder: {
+    fontSize: 16,
+    color: "#051A2E",
+    fontFamily: "karla-regular",
+    // Add more styles for placeholder
+  },
+});
 
 export default MultipleChoiceQuestion;

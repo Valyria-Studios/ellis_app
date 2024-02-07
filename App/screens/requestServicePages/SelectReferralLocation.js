bf5d8d28362177cb7902b87ac784e5d503c0a8c4
patemit <patelmit8292@gmail.com>
2024-02-07 10:35:17 -0500

    Completed Styling for Locations in SelectReferralLocation

--- App/screens/requestServicePages/SelectReferralLocation.js
+++ App/screens/requestServicePages/SelectReferralLocation.js
@@ -5,8 +5,11 @@ import {
   FlatList,
   StyleSheet,
   ActivityIndicator,
+  TouchableOpacity,
 } from "react-native";
 import globalstyles from "../../shared/globalStyles";
+import Card from "../../shared/Card";
+import { MaterialIcons, Octicons, Feather } from "@expo/vector-icons";
 
 const SelectReferralLocation = ({ route }) => {
   const { option, categoryName } = route.params;
@@ -35,32 +38,149 @@ const SelectReferralLocation = ({ route }) => {
 
   return (
     <View style={globalstyles.container}>
-      <FlatList
-        data={amenities}
-        keyExtractor={(item, index) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.amenityItem}>
-            <Text style={styles.amenityText}>
-              {item.location} - {item.address}
-            </Text>
-            {/* Render more details about each amenity as needed */}
-          </View>
-        )}
-      />
+      {amenities.map((amenity, index) => (
+        <TouchableOpacity key={index}>
+          <Card>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Text style={styles.optionText}>{option}</Text>
+              <MaterialIcons
+                name="keyboard-arrow-down"
+                size={24}
+                color={"#094852"}
+              />
+            </View>
+            <View>
+              <Text style={styles.title}>{amenity.location}</Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  marginTop: 5,
+                  alignItems: "center",
+                }}
+              >
+                <Octicons name="location" size={18} color={"#094852"} />
+                <Text style={styles.amenityText}>{amenity.address}</Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  marginTop: 5,
+                  alignItems: "center",
+                }}
+              >
+                <Feather name="clock" size={16} color={"#094852"} />
+                <Text style={styles.amenityText}>
+                  {amenity.operationalHours}
+                </Text>
+              </View>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginTop: 10,
+              }}
+            >
+              <View
+                style={[
+                  styles.enrollmentContainer,
+                  amenity.availability === "0"
+                    ? styles.waitlistContainer
+                    : styles.enrollmentContainer,
+                ]}
+              >
+                <Text
+                  style={[
+                    styles.cardAvailabilityText,
+                    amenity.availability === "0"
+                      ? styles.waitlistText
+                      : styles.enrollmentText,
+                  ]}
+                >
+                  {amenity.availability === "0"
+                    ? "Waitlist"
+                    : `${amenity.availability} Services Available`}
+                </Text>
+              </View>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <MaterialIcons
+                  name="app-registration"
+                  size={16}
+                  color={"#094852"}
+                  style={{ paddingRight: 5 }}
+                />
+                <Text style={styles.referText}>Refer</Text>
+              </View>
+            </View>
+          </Card>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  optionText: {
+    fontFamily: "gabarito-regular",
+    fontSize: 12,
+    color: "#465355",
+    letterSpacing: 1,
+    textTransform: "uppercase",
+  },
+
   title: {
+    fontFamily: "gabarito-semibold",
     fontSize: 24,
+    color: "#094852",
   },
-  amenityItem: {
-    marginTop: 10,
-    padding: 10,
-  },
+
   amenityText: {
+    fontFamily: "gabarito-regular",
+    color: "#094852",
+    fontSize: 18,
+    paddingLeft: 10,
+  },
+
+  enrollmentContainer: {
+    backgroundColor: "#E7F2F3",
+    borderWidth: 1,
+    borderColor: "#4094A2",
+    padding: 8,
+    borderRadius: 15,
+  },
+
+  waitlistContainer: {
+    backgroundColor: "#FBEFDD",
+    borderWidth: 1,
+    borderColor: "#ECAD53",
+    padding: 8,
+    borderRadius: 15,
+  },
+
+  enrollmentText: {
+    fontFamily: "karla-regular",
+    fontSize: 14,
+    color: "#094852",
+  },
+
+  waitlistText: {
+    fontFamily: "karla-regular",
+    fontSize: 14,
+    color: "#533409",
+  },
+
+  referText: {
+    fontFamily: "gabarito-regular",
     fontSize: 16,
+    color: "#094852",
+    alignItems: "center",
   },
 });
 

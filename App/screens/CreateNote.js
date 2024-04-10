2b99d7a7ce465223a90a59d392bbc611785f8716
patemit <patelmit8292@gmail.com>
2024-04-10 13:28:14 -0400

    Set values for field Notes

--- App/screens/CreateNote.js
+++ App/screens/CreateNote.js
@@ -1,80 +1,109 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+// LOGIC TO SEND TO NEXT PAGE
+
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import globalstyles from "../shared/globalStyles";
 import { TextInput } from "react-native-gesture-handler";
 import { EvilIcons, Ionicons, MaterialIcons } from "@expo/vector-icons";
 
 const CreateNote = () => {
+  const [selectedClient, setSelectedClient] = useState("");
+  const [title, setTitle] = useState("");
+  const [note, setNote] = useState("");
+  const [topicTag, setTopicTag] = useState("");
+  const [teamMember, setTeamMember] = useState("");
+
   return (
-    <View
+    <SafeAreaView
       style={[
         globalstyles.container,
-        { backgroundColor: "#FFFFFF", justifyContent: "space-between" },
+        { backgroundColor: "#FFFFFF", paddingTop: -30 },
       ]}
     >
-      <View style={styles.textInputContainer}>
-        <EvilIcons
-          name="search"
-          size={24}
-          color="#828B89"
-          style={{ paddingLeft: 10 }}
-        />
-        <TextInput
-          blurOnSubmit={true}
-          style={styles.textInput}
-          placeholder="Select Client"
-        />
+      <View style={{ justifyContent: "space-between", flex: 1 }}>
+        <View style={styles.textInputContainer}>
+          <EvilIcons
+            name="search"
+            size={24}
+            color="#828B89"
+            style={{ paddingLeft: 10 }}
+          />
+          <TextInput
+            blurOnSubmit={true}
+            style={styles.textInput}
+            placeholder="Select Client"
+            value={selectedClient}
+            onChangeText={setSelectedClient}
+          />
+        </View>
+        <View style={styles.textInputContainer}>
+          <TextInput
+            blurOnSubmit={true}
+            style={styles.textInput}
+            placeholder="Title"
+            value={title}
+            onChangeText={setTitle}
+          />
+        </View>
+        <View
+          style={[
+            styles.textInputContainer,
+            { flex: 0.75, alignItems: "flex-start" },
+          ]}
+        >
+          <TextInput
+            blurOnSubmit={true}
+            style={[styles.textInput, { paddingBottom: 410 }]}
+            multiline={true}
+            placeholder="Input Notes"
+            value={note}
+            onChangeText={setNote}
+          />
+        </View>
+        <View style={styles.textInputContainer}>
+          <MaterialIcons
+            name="label-outline"
+            size={20}
+            color="#828B89"
+            style={{ paddingLeft: 10 }}
+          />
+          <TextInput
+            blurOnSubmit={true}
+            style={styles.textInput}
+            placeholder="Topic Tags"
+            value={topicTag}
+            onChangeText={setTopicTag}
+          />
+        </View>
+        <View style={styles.textInputContainer}>
+          <Ionicons
+            name="person-outline"
+            size={20}
+            color="#828B89"
+            style={{ paddingLeft: 10 }}
+          />
+          <TextInput
+            blurOnSubmit={true}
+            style={styles.textInput}
+            placeholder="Tag a Team Member"
+            value={teamMember}
+            onChangeText={setTeamMember}
+          />
+        </View>
+        <View>
+          <TouchableOpacity
+            style={[
+              globalstyles.buttonContainer,
+              { backgroundColor: "#FFFFFF", borderRadius: 15 },
+            ]}
+            activeOpacity={0.6}
+          >
+            <Text style={[globalstyles.buttonText]}>Save Note</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={styles.textInputContainer}>
-        <TextInput
-          blurOnSubmit={true}
-          style={styles.textInput}
-          placeholder="Title"
-        />
-      </View>
-      <View
-        style={[
-          styles.textInputContainer,
-          { flex: 0.75, alignItems: "flex-start" },
-        ]}
-      >
-        <TextInput
-          blurOnSubmit={true}
-          style={styles.textInput}
-          placeholder="Input Notes"
-        />
-      </View>
-      <View style={styles.textInputContainer}>
-        <MaterialIcons
-          name="label-outline"
-          size={20}
-          color="#828B89"
-          style={{ paddingLeft: 10 }}
-        />
-        <TextInput
-          blurOnSubmit={true}
-          style={styles.textInput}
-          placeholder="Topic Tags"
-        />
-      </View>
-      <View style={styles.textInputContainer}>
-        <Ionicons
-          name="person-outline"
-          size={20}
-          color="#828B89"
-          style={{ paddingLeft: 10 }}
-        />
-        <TextInput
-          blurOnSubmit={true}
-          style={styles.textInput}
-          placeholder="Tag a Team Member"
-        />
-      </View>
-      <View>
-        <Text>Save Note</Text>
-      </View>
-    </View>
+    </SafeAreaView>
   );
 };
 

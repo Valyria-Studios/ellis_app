862a105282812072b49c8c4db0a2208516af43dd
patemit <patelmit8292@gmail.com>
2024-04-15 17:51:55 -0400

    Push notes to the Notes Model

--- App/screens/CreateClient.js
+++ App/screens/CreateClient.js
@@ -1,3 +1,5 @@
+// LOGIC FOR PUSHING PAGE
+
 import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
--- App/screens/CreateNote.js
+++ App/screens/CreateNote.js
@@ -1,18 +1,98 @@
 // LOGIC TO SEND TO NEXT PAGE
 
-import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import globalstyles from "../shared/globalStyles";
 import { TextInput } from "react-native-gesture-handler";
 import { EvilIcons, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { format } from "date-fns";
 
 const CreateNote = () => {
   const [selectedClient, setSelectedClient] = useState("");
+  const [clients, setClients] = useState([]);
+  const [filteredClients, setFilteredClients] = useState([]);
   const [title, setTitle] = useState("");
+  const [showClients, setShowClients] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [note, setNote] = useState("");
   const [topicTag, setTopicTag] = useState("");
   const [teamMember, setTeamMember] = useState("");
+
+  useEffect(() => {
+    const fetchClients = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:3000/Clients");
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+        const data = await response.json();
+        setClients(data);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    };
+
+    fetchClients();
+  }, []);
+
+  useEffect(() => {
+    // Filter clients based on input
+    if (selectedClient) {
+      const filtered = clients.filter((client) =>
+        client.fullName.toLowerCase().includes(selectedClient.toLowerCase())
+      );
+      setFilteredClients(filtered);
+    } else {
+      setFilteredClients([]);
+    }
+  }, [selectedClient, clients]);
+
+  const handleSelectClient = (client) => {
+    setSelectedClient(client.fullName);
+    setShowClients(false); // Hide the list
+  };
+
+  const handleSaveNote = async () => {
+    const now = new Date();
+    const formattedDate = format(now, "PPpp"); // Formats the current date and time
+    const newNote = {
+      title: title,
+      content: note,
+      topics: topicTag,
+      teamMember: teamMember,
+      client: selectedClient,
+      date: formattedDate,
+    };
+    console.log(newNote);
+    try {
+      const response = await fetch("http://localhost:3000/Notes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newNote),
+      });
+      if (response.ok) {
+        const responseJson = await response.json();
+        console.log("Note added successfully:", responseJson);
+        // Optionally, handle navigation or state updates here
+        // navigation.navigate("NoteDetails", { noteId: responseJson.id });
+      } else {
+        console.error("HTTP error: " + response.status + " during adding note");
+      }
+    } catch (error) {
+      console.error("Error sending data to API", error);
+    }
+  };
 
   return (
     <SafeAreaView
@@ -35,8 +115,24 @@ const CreateNote = () => {
             placeholder="Select Client"
             value={selectedClient}
             onChangeText={setSelectedClient}
+            onFocus={() => setShowClients(true)} // Show the list when input is focused
           />
         </View>
+        {showClients && filteredClients.length > 0 && (
+          <FlatList
+            data={filteredClients}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                onPress={() => handleSelectClient(item)}
+                style={styles.clientItem}
+              >
+                <Text>{item.fullName}</Text>
+              </TouchableOpacity>
+            )}
+            style={[styles.clientList, { maxHeight: 200 }]}
+          />
+        )}
         <View style={styles.textInputContainer}>
           <TextInput
             blurOnSubmit={true}
@@ -93,6 +189,7 @@ const CreateNote = () => {
         </View>
         <View>
           <TouchableOpacity
+            onPress={handleSaveNote}
             style={[
               globalstyles.buttonContainer,
               { backgroundColor: "#FFFFFF", borderRadius: 15 },
@@ -123,6 +220,23 @@ const styles = StyleSheet.create({
     fontFamily: "karla-regular",
     fontSize: 16,
     color: "#909899",
+  },
+
+  clientItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+
+  clientList: {
+    position: "absolute",
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    top: 40, // Adjust top as per your UI
+    left: 0,
+    right: 0,
+    zIndex: 1, // Make sure FlatList is rendered above the BlurView
   },
 });
 

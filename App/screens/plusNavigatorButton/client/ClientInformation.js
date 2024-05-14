b7c87e20baae89926a4c3ad69bb90b0831ed3bfd
patemit <patelmit8292@gmail.com>
2024-05-14 13:53:22 -0400

    Demographic Information Updated on Client Information Page

--- App/screens/plusNavigatorButton/client/ClientInformation.js
+++ App/screens/plusNavigatorButton/client/ClientInformation.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import globalstyles from "../../../shared/globalStyles";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -11,6 +17,12 @@ const ClientInformation = ({ navigation }) => {
     language: null,
     ethnicity: null,
     sexual_orientation: null,
+    military: null,
+    criminalJustice: null,
+    difficultyEnglish: null,
+    disability: null,
+    fosterCare: null,
+    singleParent: null,
   });
 
   const demographicInfo = [
@@ -55,8 +67,33 @@ const ClientInformation = ({ navigation }) => {
     },
   ];
 
+  const yesNoQuestions = [
+    {
+      name: "Are you currently in the military or a veteran?",
+      key: "military",
+    },
+    {
+      name: "Have you had contact with the criminal justice system?",
+      key: "criminalJustice",
+    },
+    {
+      name: "Do you have difficulty understanding English?",
+      key: "difficultyEnglish",
+    },
+    { name: "Do you have a disability?", key: "disability" },
+    {
+      name: "Are you in or have you aged out of the foster care system?",
+      key: "fosterCare",
+    },
+    { name: "Are you a single parent?", key: "singleParent" },
+  ];
+
   const createOptions = (options) => {
     return options.map((option) => ({ label: option, value: option }));
+  };
+
+  const handleYesNoChange = (key, value) => {
+    setSelectedValues((prev) => ({ ...prev, [key]: value }));
   };
 
   const handleValueChange = (value, key) => {
@@ -64,54 +101,89 @@ const ClientInformation = ({ navigation }) => {
   };
   return (
     <SafeAreaView style={globalstyles.container}>
-      <View style={{ marginTop: 60 }} />
-      <Text style={styles.subHeader}>Demographic Information</Text>
-      {demographicInfo.map((category, index) => (
-        <View key={index} style={styles.container}>
-          <View
-            style={[
-              globalstyles.optionsContainer,
-              { justifyContent: "space-between", borderWidth: 0 },
-            ]}
-          >
-            <View>
-              <RNPickerSelect
-                key={index}
-                onValueChange={(value) =>
-                  handleValueChange(value, category.key)
-                }
-                items={createOptions(category.options)}
-                placeholder={{ label: `${category.name}`, value: null }}
-                style={{
-                  input: globalstyles.input,
-                  placeholder: globalstyles.placeholder,
-                }}
-                useNativeAndroidPickerStyle={false}
-              />
-            </View>
-            <View>
-              <MaterialIcons
-                name="keyboard-arrow-right"
-                size={30}
-                style={[styles.icon, { color: "#094852" }]}
-              />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ marginTop: 60 }} />
+        <Text style={styles.subHeader}>Demographic Information</Text>
+        {demographicInfo.map((category, index) => (
+          <View key={index} style={styles.container}>
+            <View
+              style={[
+                globalstyles.optionsContainer,
+                { justifyContent: "space-between", borderWidth: 0 },
+              ]}
+            >
+              <View>
+                <RNPickerSelect
+                  key={index}
+                  onValueChange={(value) =>
+                    handleValueChange(value, category.key)
+                  }
+                  items={createOptions(category.options)}
+                  placeholder={{ label: `${category.name}`, value: null }}
+                  style={{
+                    input: globalstyles.input,
+                    placeholder: globalstyles.placeholder,
+                  }}
+                  useNativeAndroidPickerStyle={false}
+                />
+              </View>
+              <View>
+                <MaterialIcons
+                  name="keyboard-arrow-right"
+                  size={30}
+                  style={[styles.icon, { color: "#094852" }]}
+                />
+              </View>
             </View>
           </View>
+        ))}
+        {yesNoQuestions.map((question, index) => (
+          <View key={index} style={styles.yesNoContainer}>
+            <Text style={globalstyles.input}>{question.name}</Text>
+            <View style={styles.yesNoButtons}>
+              <Text style={styles.yesNoText}>Yes</Text>
+              <TouchableOpacity
+                style={[
+                  styles.radioButton,
+                  selectedValues[question.key] === "yes" &&
+                    styles.selectedRadioButton,
+                ]}
+                onPress={() => handleYesNoChange(question.key, "yes")}
+              >
+                {selectedValues[question.key] === "yes" && (
+                  <View style={styles.radioButtonInner} />
+                )}
+              </TouchableOpacity>
+              <Text style={styles.yesNoText}>No</Text>
+              <TouchableOpacity
+                style={[
+                  styles.radioButton,
+                  selectedValues[question.key] === "no" &&
+                    styles.selectedRadioButton,
+                ]}
+                onPress={() => handleYesNoChange(question.key, "no")}
+              >
+                {selectedValues[question.key] === "no" && (
+                  <View style={styles.radioButtonInner} />
+                )}
+              </TouchableOpacity>
+            </View>
+          </View>
+        ))}
+        <View>
+          <TouchableOpacity
+            style={[globalstyles.buttonContainer, { marginTop: 20 }]}
+            activeOpacity={0.8}
+            onPress={() =>
+              navigation.navigate("Request Services Page", {
+                headerTitle: "Make a Referral",
+              })
+            } // Use navigation prop to navigate
+          >
+            <Text style={[globalstyles.buttonText]}>Next</Text>
+          </TouchableOpacity>
         </View>
-      ))}
-      <View>
-        <TouchableOpacity
-          style={[globalstyles.buttonContainer, { marginTop: 20 }]}
-          activeOpacity={0.8}
-          onPress={() =>
-            navigation.navigate("Request Services Page", {
-              headerTitle: "Make a Referral",
-            })
-          } // Use navigation prop to navigate
-        >
-          <Text style={[globalstyles.buttonText]}>Next</Text>
-        </TouchableOpacity>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -130,6 +202,41 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
 
+  yesNoContainer: {
+    marginVertical: 10,
+    padding: 10,
+    borderRadius: 15,
+    // backgroundColor: "#fff",
+  },
+  yesNoText: {
+    fontSize: 16,
+    color: "#030E07",
+    marginHorizontal: 10,
+  },
+  yesNoButtons: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  radioButton: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 10,
+  },
+  selectedRadioButton: {
+    borderColor: "#094852",
+  },
+  radioButtonInner: {
+    height: 10,
+    width: 10,
+    borderRadius: 5,
+    backgroundColor: "#094852",
+  },
   icon: {
     color: "#094852",
     paddingLeft: 10,
